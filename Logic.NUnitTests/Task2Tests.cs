e9ea5717ddb687f72fb4078a0eed00704a676e0c
_shark <stanislav.kozorez@gmail.com>
2016-03-14 02:38:07 +0300

    Task2 with NUnit tests is done

---
+++ Logic.NUnitTests/Task2Tests.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System.Diagnostics;
+
+namespace Logic.NUnitTests
+{
+    [TestFixture]
+    public class Task2Tests
+    {
+        #region GCD tests
+        [TestCase(0, 0, ExpectedException = typeof(ArgumentException))]
+        [TestCase(0, -4, Result = 4)]
+        [TestCase(5, 0, Result = 5)]
+        [TestCase(1, 173, Result = 1)]
+        [TestCase(466, 1, Result = 1)]
+        [TestCase(17, 17, Result = 17)]
+        public int GCDTestWith2Params(int firstValue, int secondValue)
+        {
+            long elapsed;
+            int result = Task2.GCD(out elapsed, firstValue, secondValue);
+            Debug.WriteLine($"Elapsed time: {elapsed}");
+
+            return result;
+        }
+
+        [TestCase(0, 0, 5, ExpectedException = typeof(ArgumentException))]
+        [TestCase(0, -4, 10, Result = 2)]
+        [TestCase(5, 0, 0, Result = 5)]
+        [TestCase(1, 173, 35, Result = 1)]
+        [TestCase(466, 1, 22, Result = 1)]
+        [TestCase(17, 17, 17, Result = 17)]
+        [TestCase(17453, 123567, 13477, Result = 1)]
+        public int GCDTestWith3Params(int firstValue, int secondValue, int thirdValue)
+        {
+            long elapsed;
+            int result = Task2.GCD(out elapsed, firstValue, secondValue, thirdValue);
+            Debug.WriteLine($"Elapsed time: {elapsed}");
+
+            return result;
+        }
+
+        [TestCase(4, ExpectedException = typeof(ArgumentException))]
+        [TestCase(0, 0, 5, 10, ExpectedException = typeof(ArgumentException))]
+        [TestCase(0, -4, 10, 2, Result = 2)]
+        [TestCase(5, 0, 0, 0, Result = 5)]
+        [TestCase(1, 173, 35, 44, Result = 1)]
+        [TestCase(466, 1, 22, 14, Result = 1)]
+        [TestCase(17, 17, 17, 17, Result = 17)]
+        public int GCDTestWithParams(params int[] values)
+        {
+            long elapsed;
+            int result = Task2.GCD(out elapsed, values);
+            Debug.WriteLine($"Elapsed time: {elapsed}");
+
+            return result;
+        }
+
+        [Test, ExpectedException(typeof(ArgumentNullException))]
+        public void GCDTestWithNullParams()
+        {
+            long elapsed;
+            int[] args = null;
+            int result = Task2.GCD(out elapsed, args);
+            Debug.WriteLine($"Elapsed time: {elapsed}");
+        }
+
+        #endregion
+
+        #region Binary GCD tests
+
+        [TestCase(0, 0, ExpectedException = typeof(ArgumentException))]
+        [TestCase(0, -4, Result = 4)]
+        [TestCase(5, 0, Result = 5)]
+        [TestCase(1, 173, Result = 1)]
+        [TestCase(466, 1, Result = 1)]
+        [TestCase(17, 17, Result = 17)]
+        public int BinaryGCDTestWith2Params(int firstValue, int secondValue)
+        {
+            long elapsed;
+            int result = Task2.BinaryGCD(out elapsed, firstValue, secondValue);
+            Debug.WriteLine($"Elapsed time: {elapsed}");
+
+            return result;
+        }
+
+        [TestCase(0, 0, 5, ExpectedException = typeof(ArgumentException))]
+        [TestCase(0, -4, 10, Result = 2)]
+        [TestCase(5, 0, 0, Result = 5)]
+        [TestCase(1, 173, 35, Result = 1)]
+        [TestCase(466, 1, 22, Result = 1)]
+        [TestCase(17, 17, 17, Result = 17)]
+        [TestCase(17453, 123567, 13477, Result = 1)]
+        public int BinaryGCDTestWith3Params(int firstValue, int secondValue, int thirdValue)
+        {
+            long elapsed;
+            int result = Task2.BinaryGCD(out elapsed, firstValue, secondValue, thirdValue);
+            Debug.WriteLine($"Elapsed time: {elapsed}");
+
+            return result;
+        }
+
+        [TestCase(4, ExpectedException = typeof(ArgumentException))]
+        [TestCase(0, 0, 5, 10, ExpectedException = typeof(ArgumentException))]
+        [TestCase(0, -4, 10, 2, Result = 2)]
+        [TestCase(5, 0, 0, 0, Result = 5)]
+        [TestCase(1, 173, 35, 44, Result = 1)]
+        [TestCase(466, 1, 22, 14, Result = 1)]
+        [TestCase(17, 17, 17, 17, Result = 17)]
+        public int BinaryGCDTestWithParams(params int[] values)
+        {
+            long elapsed;
+            int result = Task2.BinaryGCD(out elapsed, values);
+            Debug.WriteLine($"Elapsed time: {elapsed}");
+
+            return result;
+        }
+
+        [Test, ExpectedException(typeof(ArgumentNullException))]
+        public void BinaryGCDTestWithNullParams()
+        {
+            long elapsed;
+            int[] args = null;
+            int result = Task2.BinaryGCD(out elapsed, args);
+            Debug.WriteLine($"Elapsed time: {elapsed}");
+        }
+
+        #endregion
+    }
+}
---
+++ Logic/Task2.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public static class Task2
+    {
+        private static Stopwatch timer = new Stopwatch();
+
+        #region Euclidean algorithm
+        public static int GCD(out long elapsedMilliseconds, int firstValue, int secondValue)
+        {
+            int result = 0;
+            elapsedMilliseconds = 0;
+
+            timer.Restart();
+            result = GCDImplementation(Math.Abs(firstValue), Math.Abs(secondValue));
+            timer.Stop();
+            elapsedMilliseconds = timer.ElapsedMilliseconds;
+
+            return result;
+        }
+
+        public static int GCD(out long elapsedMilliseconds, int firstValue, int secondValue, int thirdValue)
+        {
+            int result = 0;
+            elapsedMilliseconds = 0;
+
+            timer.Restart();
+            result = GCDImplementation(GCDImplementation(Math.Abs(firstValue), Math.Abs(secondValue)), Math.Abs(thirdValue));
+            timer.Stop();
+            elapsedMilliseconds = timer.ElapsedMilliseconds;
+
+            return result;
+        }
+
+        public static int GCD(out long elapsedMilliseconds, params int[] values)
+        {
+            int result = 0;
+            elapsedMilliseconds = 0;
+
+            if (values == null)
+                throw new ArgumentNullException($"parameter {nameof(values)} is null");
+            if (values.Length < 2)
+                throw new ArgumentException("There's lack of parameters");
+            timer.Restart();
+            result = Math.Abs(values[0]);
+            for (int i = 1; i < values.Length; i++)
+            {
+                result = GCDImplementation(result, Math.Abs(values[i]));
+            }
+            timer.Stop();
+            elapsedMilliseconds = timer.ElapsedMilliseconds;
+
+            return result;
+        }
+
+        private static int GCDImplementation(int firstValue, int secondValue)
+        {
+            if (firstValue == 0 && secondValue == 0)
+                throw new ArgumentException("Unable to find the GCD of zero values");
+            if (firstValue == 0)
+                return secondValue;
+            if (secondValue == 0)
+                return firstValue;
+            if (firstValue == secondValue)
+                return firstValue;
+            if (firstValue == 1 || secondValue == 1)
+                return 1;
+
+            while (firstValue != secondValue)
+            {
+                if (firstValue > secondValue)
+                    firstValue = firstValue - secondValue;
+                else
+                    secondValue = secondValue - firstValue;
+            }
+            return firstValue;
+        }
+        #endregion
+
+        #region Binary Euclidean algorithm
+
+        public static int BinaryGCD(out long elapsedMilliseconds, int firstValue, int secondValue)
+        {
+            int result = 0;
+            elapsedMilliseconds = 0;
+
+            timer.Restart();
+            result = BinaryGCDImplementation(Math.Abs(firstValue), Math.Abs(secondValue));
+            timer.Stop();
+            elapsedMilliseconds = timer.ElapsedMilliseconds;
+
+            return result;
+        }
+
+        public static int BinaryGCD(out long elapsedMilliseconds, int firstValue, int secondValue, int thirdValue)
+        {
+            int result = 0;
+            elapsedMilliseconds = 0;
+
+            timer.Restart();
+            result = BinaryGCDImplementation(BinaryGCDImplementation(Math.Abs(firstValue), Math.Abs(secondValue)), Math.Abs(thirdValue));
+            timer.Stop();
+            elapsedMilliseconds = timer.ElapsedMilliseconds;
+
+            return result;
+        }
+
+        public static int BinaryGCD(out long elapsedMilliseconds, params int[] values)
+        {
+            int result = 0;
+            elapsedMilliseconds = 0;
+
+            if (values == null)
+                throw new ArgumentNullException($"parameter {nameof(values)} is null");
+            if (values.Length < 2)
+                throw new ArgumentException("There's lack of parameters");
+            timer.Restart();
+            result = Math.Abs(values[0]);
+            for (int i = 1; i < values.Length; i++)
+            {
+                result = BinaryGCDImplementation(result, Math.Abs(values[i]));
+            }
+            timer.Stop();
+            elapsedMilliseconds = timer.ElapsedMilliseconds;
+
+            return result;
+        }
+
+        private static int BinaryGCDImplementation(int firstValue, int secondValue)
+        {
+            int result = 0;
+
+            if (firstValue == 0 && secondValue == 0)
+                throw new ArgumentException("Unable to find the GCD of zero values");
+            if (firstValue == 0)
+                return secondValue;
+            if (secondValue == 0)
+                return firstValue;
+            if (firstValue == secondValue)
+                return firstValue;
+            if (firstValue == 1 || secondValue == 1)
+                return 1;
+            if (firstValue % 2 == 0 && secondValue % 2 == 0)
+                result = 2 * BinaryGCDImplementation(firstValue / 2, secondValue / 2);
+            if (firstValue % 2 == 0 && secondValue % 2 == 1)
+                result = BinaryGCDImplementation(firstValue / 2, secondValue);
+            if (firstValue % 2 == 1 && secondValue % 2 == 0)
+                result = BinaryGCDImplementation(firstValue, secondValue / 2);
+            if (firstValue % 2 == 1 && secondValue % 2 == 1)
+                if (secondValue > firstValue)
+                    result = BinaryGCDImplementation((secondValue - firstValue) / 2, firstValue);
+                else
+                    result = BinaryGCDImplementation((firstValue - secondValue) / 2, secondValue);
+
+            return result;
+        }
+
+
+        #endregion
+    }
+}
