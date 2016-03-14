adccacb0caca375c608f60e70949ea7c8b28ebae
_shark <stanislav.kozorez@gmail.com>
2016-03-14 06:08:41 +0300

    Task 3 and MSUnit tests are done

---
+++ Logic.MSUnitTests/HexFormatterTests.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Logic.MSUnitTests
+{
+    [TestClass]
+    public class HexFormatterTests
+    {
+        [TestMethod]
+        public void Format_SByteValueAndUppercaseFormat_ReturnedUppercaseHexString()
+        {
+            sbyte initial = 27;
+            string expected = "0X1B";
+
+            string actual = String.Format(new HexFormatter(), "{0:H}", initial);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Format_SByteValueAndLowercaseFormat_ReturnedLowercaseHexString()
+        {
+            sbyte initial = -27;
+            string expected = "-0x1b";
+
+            string actual = String.Format(new HexFormatter(), "{0:h}", initial);
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void Format_ByteValueAndUppercaseFormat_ReturnedUppercaseHexString()
+        {
+            byte initial = 95;
+            string expected = "0X5F";
+
+            string actual = String.Format(new HexFormatter(), "{0:H}", initial);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Format_ByteValueAndLowercaseFormat_ReturnedLowercaseHexString()
+        {
+            byte initial = 95;
+            string expected = "0x5f";
+
+            string actual = String.Format(new HexFormatter(), "{0:h}", initial);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Format_UShortValueAndUppercaseFormat_ReturnedUppercaseHexString()
+        {
+            ushort initial = 512;
+            string expected = "0X200";
+
+            string actual = String.Format(new HexFormatter(), "{0:H}", initial);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Format_UShortValueAndLowercaseFormat_ReturnedLowercaseHexString()
+        {
+            ushort initial = 512;
+            string expected = "0x200";
+
+            string actual = String.Format(new HexFormatter(), "{0:h}", initial);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Format_shortValueAndUppercaseFormat_ReturnedUppercaseHexString()
+        {
+            short initial = -754;
+            string expected = "-0X2F2";
+
+            string actual = String.Format(new HexFormatter(), "{0:H}", initial);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Format_shortValueAndLowercaseFormat_ReturnedLowercaseHexString()
+        {
+            short initial = 754;
+            string expected = "0x2f2";
+
+            string actual = String.Format(new HexFormatter(), "{0:h}", initial);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Format_UintValueAndUppercaseFormat_ReturnedUppercaseHexString()
+        {
+            uint initial = 1564;
+            string expected = "0X61C";
+
+            string actual = String.Format(new HexFormatter(), "{0:H}", initial);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Format_UIntValueAndLowercaseFormat_ReturnedLowercaseHexString()
+        {
+            uint initial = 1564;
+            string expected = "0x61c";
+
+            string actual = String.Format(new HexFormatter(), "{0:h}", initial);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Format_IntValueAndUppercaseFormat_ReturnedUppercaseHexString()
+        {
+            int initial = -1195;
+            string expected = "-0X4AB";
+
+            string actual = String.Format(new HexFormatter(), "{0:H}", initial);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Format_IntValueAndLowercaseFormat_ReturnedLowercaseHexString()
+        {
+            int initial = 1195;
+            string expected = "0x4ab";
+
+            string actual = String.Format(new HexFormatter(), "{0:h}", initial);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Format_LongValueAndUppercaseFormat_ReturnedUppercaseHexString()
+        {
+            long initial = -11165495;
+            string expected = "-0XAA5F37";
+
+            string actual = String.Format(new HexFormatter(), "{0:H}", initial);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Format_LongValueAndLowercaseFormat_ReturnedLowercaseHexString()
+        {
+            long initial = 11165495;
+            string expected = "0xaa5f37";
+
+            string actual = String.Format(new HexFormatter(), "{0:h}", initial);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Format_ULongValueAndUppercaseFormat_ReturnedUppercaseHexString()
+        {
+            ulong initial = 4687945883;
+            string expected = "0X1176C609B";
+
+            string actual = String.Format(new HexFormatter(), "{0:H}", initial);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Format_ULongValueAndLowercaseFormat_ReturnedLowercaseHexString()
+        {
+            ulong initial = 4687945883;
+            string expected = "0x1176c609b";
+
+            string actual = String.Format(new HexFormatter(), "{0:h}", initial);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Logic/Task3/HexFormatter.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class HexFormatter: IFormatProvider, ICustomFormatter
+    {
+        private readonly List<Type> supportedTypes;
+
+        public HexFormatter()
+        {
+            supportedTypes = new List<Type>()
+            {
+                typeof(sbyte),
+                typeof(byte),
+                typeof(int),
+                typeof(short),
+                typeof(long),
+                typeof(ushort),
+                typeof(uint),
+                typeof(ulong)
+            };
+        }
+
+        public object GetFormat(Type formatType)
+        {
+            if (formatType == typeof(ICustomFormatter))
+                return this;
+            else
+                return null;
+        }
+
+        public string Format(string format, object arg, IFormatProvider formatProvider)
+        {
+            string hexFormat = string.Empty;
+            string resultFormat = string.Empty;
+            string prefix = string.Empty;
+
+            if (!supportedTypes.Contains(arg.GetType()))
+                try
+                {
+                    return HandleOtherFormats(format, arg);
+                }
+                catch (FormatException e)
+                {
+                    throw new FormatException(String.Format("The format of '{0}' is invalid.", format), e);
+                }
+            if (!String.IsNullOrEmpty(format))
+                hexFormat = format.Length > 1 ? format.Substring(0, 1) : format;
+            if (Double.Parse(arg.ToString()) < 0)
+            {
+                prefix = "-";
+                arg = long.Parse(arg.ToString()) * -1;
+            }
+            if (hexFormat == "H")
+            {
+                prefix += "0X";
+                resultFormat = "{0:X}";
+            }
+            else
+                if (hexFormat == "h")
+                {
+                    prefix += "0x";
+                    resultFormat = "{0:x}";
+                }
+                else
+                {
+                    try
+                    {
+                        return HandleOtherFormats(format, arg);
+                    }
+                    catch (FormatException e)
+                    {
+                        throw new FormatException(String.Format("The format of '{0}' is invalid.", format), e);
+                    }
+                }
+
+            return prefix + String.Format(resultFormat, arg);
+        }
+
+        private string HandleOtherFormats(string format, object arg)
+        {
+            if (arg is IFormattable)
+                return ((IFormattable)arg).ToString(format, CultureInfo.CurrentCulture);
+            else if (arg != null)
+                return arg.ToString();
+            else
+                return String.Empty;
+        }
+    }
+}
